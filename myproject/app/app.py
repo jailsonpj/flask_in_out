45b10a3d16e12a0f463fe9830525ab8d0f8c4e0d
jailsonpj <jailsoncolares@gmail.com>
2017-09-07 21:18:31 -0400

    Update app.py

--- myproject/app/app.py
+++ myproject/app/app.py
@@ -4,6 +4,7 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
+from forms import CadastroFuncionarioForm
 
 app = Flask(__name__)
 
@@ -63,32 +64,10 @@ def article(id):
 
     return render_template('article.html', article=article)
 
-
-# Register Form Class
-class RegisterForm(Form):
-    funcionario_id = StringField('ID', [validators.Length(min=1, max=12)])
-    funcionario_nome = StringField('Name', [validators.Length(min=1, max=50)])
-    funcionario_login = StringField('Username', [validators.Length(min=4, max=50)])
-    funcionario_senha = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
-    funcionario_email = StringField('Email', [validators.Length(min=6, max=50)])
-    funcionario_status = StringField('Status (-1, 0, 1)', [validators.Length(min=1 , max=1)])
-    funcionario_matricula = StringField('Matrícula', [validators.Length(min=6, max=12)])
-    funcionario_ultimo_acesso = StringField('Ultimo acesso', [validators.Length(min=10, max=10)])
-    funcionario_enviados = StringField('Enviados', [validators.Length(min=1, max=100)])
-    funcionario_recebidos = StringField('Recebidos', [validators.Length(min=1, max=100)])
-    funcionario_telefone = StringField('Telefone', [validators.Length(min=9, max=9)])
-    funcionario_unidade = StringField('Unidade', [validators.Length(min=2, max=100)])
-   
-
-
-# User Register
+# Funcionário Cadastro
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
+    form = FuncionarioCadastroForm(request.form)
     if request.method == 'POST' and form.validate():
         
         funcionario_id = form.funcionario_id.data
